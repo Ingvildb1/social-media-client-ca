e468e1449f6ea0f7ab7cef158ea315e56b430b44
Ingvildb1 <111681292+Ingvildb1@users.noreply.github.com>
2023-04-16 11:43:06 +0200

    e2e testing invalid login, valid login, logout

---
+++ cypress/e2e/3-testing/invalidlogin.cy.js
@@ -0,0 +1,22 @@
+describe("NOT authenticated user", () => {
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("header [data-auth='login']").click({ force: true });
+  });
+
+  it("CAN NOT log in with invalid credentials and is shown a message", () => {
+    cy.get("#loginForm #loginEmail").type("failtest@noroff.no");
+    cy.get("#loginForm #loginPassword").type("failtest1234");
+    cy.get('#loginForm button[type="submit"]').click({ force: true });
+    cy.wait(1000);
+
+    cy.get("#loginForm .btn-close").click();
+    cy.on("window:alert", (Text) => {
+      expect(Text).to.eq(
+        "Your e-mail or password is incorrect. Please try again"
+      );
+    });
+  });
+});
